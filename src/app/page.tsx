517c6b12f9c7b450022700d7d56a12baefd1c9e5
yoannclaisse <claisse.yoann@gmail.com>
2024-05-28 15:36:18 +0200

    add redirect in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,20 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { getAllUsers, GetUserWithTodosById } from "./lib/user/user.method";
-import { log } from "console";
+import Link from "next/link";
 
-export default async function Home() {
-  const rqAllUsers = await getAllUsers()
-  const {allUsers} = rqAllUsers
-  console.log(allUsers)
-
-
-  const rqAllUsersByTodo = await GetUserWithTodosById(2)
-  const {userWithTodosById} = rqAllUsersByTodo.user
-  console.log("toto", userWithTodosById)
-
+export default async function HomePage() {
 
   return (
     <main >
       <div>
-        {userWithTodosById.todos.map(
-          (todo: any)=>(
-            <div>
-            {todo.description}
-            </div>
-          )
-        )}
-      </div>
+      <h1>Bienvenue sur la page d'accueil</h1>
+      <Link href="/login">
+        Aller à la page de connexion
+      </Link>
+      <Link href="/signin">
+        Aller à la page de création de compte
+      </Link>
+    </div>
     </main>
   );
 }
