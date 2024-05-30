3a02d5c7da5567903321ef55018947ac6e2aae96
yoannclaisse <claisse.yoann@gmail.com>
2024-05-30 10:45:38 +0200

    add dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,49 @@
-export default function About() {
-    return <div>About</div>
+"use client";
+import { useRouter } from "next/navigation";
+
+const DashboardPage = () => {
+  const userId = localStorage.getItem("userId");
+  const { push } = useRouter();
+  if (!userId) {
+    push(`/`);
   }
+
+  const handleLogin = async (userId: Number) => {
+    console.log(userId)
+    try {
+      const response = await fetch("http://localhost:3001/graphql", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          operationName: null,
+          variables: {
+            userId: userId,
+          },
+          query: `query userById($userId: Int!) {
+            userById(id: $userId) {
+              id
+              email
+              username
+                todos {
+                  id
+                  description
+                }
+            }
+          }`,
+        }),
+      });
+
+      const json = await response.json();
+      const data = json.data;
+      console.log(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  handleLogin(Number(userId))
+  return <div>Dashboard</div>;
+};
+
+export default DashboardPage;
