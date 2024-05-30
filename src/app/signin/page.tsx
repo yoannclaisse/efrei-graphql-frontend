bb3e93aa4dc694bb0f89c3326275d87dbb7ceac8
yoannclaisse <claisse.yoann@gmail.com>
2024-05-30 10:45:15 +0200

    add signin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,6 +1,7 @@
-'use client'
-import React, { useState } from 'react';
-import { AddUser } from '../lib/user/user.method'; // Assurez-vous que le chemin est correct
+"use client";
+import React, { useState } from "react";
+import { AddUser } from "../lib/user/user.method"; // Assurez-vous que le chemin est correct
+import Link from "next/link";
 
 interface UserCreateInput {
   username: string;
@@ -9,9 +10,9 @@ interface UserCreateInput {
 }
 
 const AddUserForm = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -20,46 +21,53 @@ const AddUserForm = () => {
 
     try {
       const { user } = await AddUser(inputCreateUser);
-      console.log('User added:', user);
+      console.log("User added:", user);
     } catch (error) {
-      console.error('Error adding user:', error);
+      console.error("Error adding user:", error);
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">Username: </label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email: </label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password: </label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit">Add User</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Add User</button>
+      </form>
+      <Link href="/">
+        <button>
+          Back to Home
+        </button>
+      </Link>
+    </div>
   );
 };
 
