672cc2e496020af8c3baebd86cc12c1e491ccecd
yoannclaisse <claisse.yoann@gmail.com>
2024-05-30 12:35:31 +0200

    clean code and add component button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,45 +2,13 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import LoginButton from "../component/LoginButton";
+import BackToHomeButton from "../component/backToHommeButton";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { push } = useRouter();
-
-  const handleLogin = async () => {
-    try {
-      const response = await fetch("http://localhost:3001/graphql", {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({
-          operationName: null,
-          variables: {
-            username: username,
-            password: password,
-          },
-          query:
-            "query ($username: String!, $password: String!) {\n  getUserId(username: $username, password: $password)\n}\n",
-        }),
-      });
-
-      const json = await response.json();
-      const data = json.data;
-      console.log("DATA LOGIN PAGE", data);
-
-      if (!!data && !!data.getUserId) {
-        // push(`/dashboard?username=${username}`);
-        localStorage.setItem('userId', data.getUserId)
-        push(`/dashboard`);
-      } else {
-        alert("Identifiants incorrects");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
 
   return (
     <div>
@@ -58,17 +26,11 @@ const LoginPage = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button onClick={handleLogin}>Login</button>
+        <LoginButton />
       </div>
-      <Link href="/">
-        <button>
-          Back to Home
-        </button>
-      </Link>
+      <BackToHomeButton />
     </div>
   );
 };
 
 export default LoginPage;
-
-
