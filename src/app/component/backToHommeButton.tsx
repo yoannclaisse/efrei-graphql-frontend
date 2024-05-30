45128f0560ca45a41e90c4388737c4e3f722b59a
yoannclaisse <claisse.yoann@gmail.com>
2024-05-30 12:34:51 +0200

    add bacToHome component

---
+++ src/app/component/backToHommeButton.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const BackToHomeButton = () => {
+  return (
+    <Link href="/">
+        <button>Back to Home</button>
+    </Link>
+  );
+};
+
+export default BackToHomeButton;
