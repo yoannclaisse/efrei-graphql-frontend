9b8391eb54b0286d436bfef58beec937b3434c6b
yoannclaisse <claisse.yoann@gmail.com>
2024-06-13 16:50:45 +0200

    change fetch call to apollo calls

--- src/app/utils/todoService.ts
+++ src/app/utils/todoService.ts
@@ -1,33 +1,29 @@
+import { gql } from "@apollo/client";
+import apolloClient from "../lib/apollo.server"
+
+const ADD_TODO_MUTATION = gql`
+  mutation CreateTodo($data: CreateTodoInput!) {
+    createTodo(data: $data) {
+      title
+      description
+      id
+    }
+  }
+`;
+
 export const addTodo = async (title: string, description: string, userId: number) => {
     try {
-        const response = await fetch("http://localhost:3001/graphql", {
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-            },
-            body: JSON.stringify({
-                operationName: "CreateTodo",
-                variables: {
-                    data: {
-                        title: title,
-                        description: description,
-                        userId: userId
-                    }
-                },
-                query: `
-          mutation CreateTodo($data: CreateTodoInput!) {
-            createTodo(data: $data) {
-              title
-              description
-              id
+        const { data } = await apolloClient.mutate({
+            mutation: ADD_TODO_MUTATION,
+            variables: {
+                data: {
+                    title: title,
+                    description: description,
+                    userId: userId,
+                }
             }
-          }
-          `,
-            }),
         });
 
-        const json = await response.json();
-        const data = json.data;
         console.log("DATA TODO CREATION", data);
 
         if (data && data.createTodo) {
@@ -41,83 +37,66 @@ export const addTodo = async (title: string, description: string, userId: number
     }
 };
 
-export const updateTodo = async (todoId: any, newTitle: any, newDescription: any) => {
-    try {
-        const response = await fetch("http://localhost:3001/graphql", {
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-            },
-            body: JSON.stringify({
-                operationName: "UpdateTodo",
-                variables: {
-                    id: todoId,
-                    title: newTitle,
-                    description: newDescription,
-                  },
-                  query: `
-                    mutation UpdateTodo($id: Int!, $title: String!, $description: String) {
-                      updateTodoById(id: $id, title: $title, description: $description) {
-                        id
-                        title
-                        description
-                      }
-                    }
-                  `,
-            }),
-        });
-
-        const json = await response.json();
-        const data = json.data;
-        console.log("DATA TODO UPDATE", data);
-
-        if (!!data && !!data.updateTodoById) {
-            return data.updateTodoById;
-        } else {
-            throw new Error("Erreur lors de la mise à jour de la todo");
-        }
-    } catch (error) {
-        console.error("Error:", error);
-        throw error;
+const UPDATE_TODO_MUTATION = gql`
+  mutation UpdateTodo($id: Int!, $title: String!, $description: String) {
+    updateTodoById(id: $id, title: $title, description: $description) {
+      id
+      title
+      description
     }
-};
+  }
+`;
+
+export const updateTodo = async (todoId: number, newTitle: string, newDescription: string) => {
+    try {
+      const { data } = await apolloClient.mutate({
+        mutation: UPDATE_TODO_MUTATION,
+        variables: {
+          id: todoId,
+          title: newTitle,
+          description: newDescription,
+        },
+      });
+  
+      console.log("DATA TODO UPDATE", data);
+  
+      if (!!data && !!data.updateTodoById) {
+        return data.updateTodoById;
+      } else {
+        throw new Error("Erreur lors de la mise à jour de la todo");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
+  };
+
+const DELETE_TODO_MUTATION = gql`
+  mutation DeleteTodo($id: Int!) {
+    deleteTodoById(id: $id) {
+      id
+    }
+  }
+`;
 
 export const deleteTodo = async (todoId: number) => {
     try {
-        const response = await fetch("http://localhost:3001/graphql", {
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-            },
-            body: JSON.stringify({
-                operationName: "DeleteTodo",
-                variables: { id: todoId },
-                query: `
-          mutation DeleteTodo($id: Int!) {
-            deleteTodoById(id: $id) {
-              id
-            }
-          }
-          `,
-            }),
-        });
-
-        const json = await response.json();
-        const data = json.data;
-        console.log("DATA TODO DELETION", data);
-
-        if (!!data && !!data.deleteTodoById) {
-            return data.deleteTodoById;
-        } else {
-            throw new Error("Erreur lors de la suppression de la todo");
-        }
+      const { data } = await apolloClient.mutate({
+        mutation: DELETE_TODO_MUTATION,
+        variables: {
+          id: todoId,
+        },
+      });
+  
+      console.log("DATA TODO DELETION", data);
+  
+      if (!!data && !!data.deleteTodoById) {
+        return data.deleteTodoById;
+      } else {
+        throw new Error("Erreur lors de la suppression de la todo");
+      }
     } catch (error) {
-        console.error("Error:", error);
-        throw error;
+      console.error("Error:", error);
+      throw error;
     }
-};
-
-
-
-
-
+  };
